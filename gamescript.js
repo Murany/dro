c2a407c5db83232d338f945ea3a1892c1a83c59b
Szeidermann Richard <szeidermann@gmail.com>
2020-11-09 21:34:02 +0100

    customer-cycle

--- gamescript.js
+++ gamescript.js
@@ -78,15 +78,21 @@ $(document).ready(function(){
 });
 });
 });*/
-var money = "0000";
-var lvl = "00";
+var money = 0;
+var lvl = 0;
+var game_mode = "auto";
 var time_limit = null;
+var time_repeat = null;
 var pets = [{image:"images/pets/cat1.png",width:119,height:105}, {image:"images/pets/dog1.png",width:140,height:122}];
 var ownerheads = [{image:"images/heads/o_h1.png",width:63,height:78}, {image:"images/heads/o_h2.png",width:35,height:56}, {image:"images/heads/o_h3.png",width:49,height:56}];
 var ownertorsos = [{image:"images/torsos/o_t1.png",width:63,height:81,cutoff:18}, {image:"images/torsos/o_t2.png",width:63,height:70}, {image:"images/torsos/o_t3.png",width:63,height:63,cutoff:7}];
 var ownerlegs = [{image:"images/legs/o_l1.png",width:63,height:105}, {image:"images/legs/o_l2.png",width:57,height:105}, {image:"images/legs/o_l3.png",width:63,height:84}];	
 var colors = [{image:"images/s_red.png",code:"#c43810"}, {image:"images/s_black.png",code:"#000000"},  {image:"images/s_blue.png",code:"#1747ba"},  {image:"images/s_green.png",code:"#36b60f"}, {image:"images/s_orange.png",code:"#d06612"} ];
 var curr_illnesses = new Array;
+var curr_customer = {
+	pixels_to_reach : 0,
+	money:0,
+};
 game_start();
 	//var audio = new Audio('sss_theme.mp3');
 
@@ -97,17 +103,55 @@ function game_start() {
 	$("#loading").hide();
 	//audio.loop=true;
 	//audio.play();
-
-	$("#lvl-value").html(lvl);
-	$("#money-value").html(money);
+	var m_padded = create_padded_string(money.toString(),"0",4);
+	var l_padded = create_padded_string(lvl.toString(),"0",2);
+	$("#money-value").html(m_padded);
+	$("#lvl-value").html(l_padded);
+	$("#start-next").show();
+	$("#game-mode").html("mode: "+game_mode);
 	
-	create_customer();
+}
+
+function create_padded_string(str,padStr, len) {
+	while (str.length < len)
+	   str = padStr + str;
+	return str;
+}
+
+
+function set_money(amount,addition){
+	var sign = (addition)?"+":"-";
+	$("#money-change").show();
+	$("#money-change").html(sign+amount);
+	(addition)?money+=amount:money-=amount;
+	money_string = money.toString();
+	var padded = (money>0)?create_padded_string(money_string,"0",4):money_string;
+	$("#money-value").html(padded);
+	setTimeout(function(){
+		$("#money-change").hide();
+	},1500)
+}
+function create_money(){
+	var max = 0;
+	if(lvl < 3){
+		max = 50;
+	}
+	else if(6 >lvl >3 )
+	{
+		max = 100;
+	}
+	else if(9 >lvl >6 )
+	{
+		max = 150;
+	}
+	
+	return Math.floor(Math.random() * max) + 1 
 }
 function create_illness() {
 	
 	var s_length = 0;
 	var milliseconds = 0;
-	var last_color = "";
+	var last_color = null;
 	var arr_illnesses = [];
 	if(lvl < 3){
 		s_length = 3;
@@ -129,7 +173,11 @@ function create_illness() {
 	}
 	for (i = 0; i < s_length; i++) {
 		var c = colors[Math.floor(Math.random() * colors.length)];
-		arr_illnesses.push(c);
+		if (c !== last_color){
+			arr_illnesses.push(c);
+		}
+		last_color = c;
+		
 	}
 	time_limit = new Date(milliseconds);
 	
@@ -156,29 +204,61 @@ function shuffle(array) {
 
 function start_timer(){
 	$('#time-limit').show();
-	var repeat = setInterval(function(){
+	time_repeat = setInterval(function(){
 		if(time_limit.getTime() > 0 ){
 			
 			time_limit = new Date(time_limit-1000);
 			$("#time-limit").html(time_limit.getMinutes()+":"+time_limit.getSeconds());
 		}
 		else{
-			clearInterval(repeat);
+			clearInterval(time_repeat);
 			$('#time-limit').hide();
-			customer_leave(false);
+			set_money(curr_customer.money,false);
+			move_costumer(curr_customer.pixels_to_reach,true);
 		}
 	},1000);
 }
 
-function customer_leave(success){
-	/*bg flip
-	-moz-transform: scaleX(-1);
-    -o-transform: scaleX(-1);
-    -webkit-transform: scaleX(-1);
-    transform: scaleX(-1);
-    filter: FlipH;
-    -ms-filter: "FlipH";
-	*/
+function move_costumer(amount,positive){
+	
+	var string_val = ((positive)?"+":"-")+"=";
+	if(positive){ //leaving
+		$('.bg-to-flip').each(function( index ) {
+			$( this ).css({"-moz-transform" : "scaleX(-1)" , "-o-transform" : "scaleX(-1)" , "transform" : "scaleX(-1)", "filter" :  "FlipH", "-ms-filter":"FlipH"})
+		});
+	}
+	else{
+		$('.bg-to-flip').each(function( index ) {
+			$( this ).css({"-moz-transform" : "scaleX(1)" , "-o-transform" : "scaleX(1)" , "transform" : "scaleX(1)", "filter" :  "FlipH", "-ms-filter":"FlipH"})
+		});
+	}
+	
+	$( "#customer" ).animate({
+    left: string_val+amount,}, {duration: 3300,specialEasing: {width: "linear"},
+		complete: function() {
+			if(!positive){	// arriwing	
+				create_illness();
+			}else{
+				setTimeout(function(){
+				reset_syringes();
+				if(game_mode == "auto"){
+					create_customer();
+				}
+				else{
+					$("#start-next").show();
+				}
+				},2500);
+			}
+		}
+	});
+	
+}
+
+function reset_syringes(){
+	$('#selected-syringe').hide();
+	$('.syringe').css("background-image","url('images/syringe.png')");
+	$('.code-wrap').show();
+	$('.color-code').remove();
 }
 
 function tell_illnesses(illnesses) {
@@ -242,9 +322,9 @@ function create_medicine(illnesses){
 
 function create_customer() {
 	var pet = pets[Math.floor(Math.random() * pets.length)];
-	var head = ownerheads[2]; //ownerheads[Math.floor(Math.random() * ownerheads.length)];
-	var torso = ownertorsos[2]; //ownertorsos[Math.floor(Math.random() * ownertorsos.length)];
-	var legs = ownerlegs[2]; //ownerlegs[Math.floor(Math.random() * ownerlegs.length)];
+	var head = ownerheads[Math.floor(Math.random() * ownerheads.length)];
+	var torso = ownertorsos[Math.floor(Math.random() * ownertorsos.length)];
+	var legs = ownerlegs[Math.floor(Math.random() * ownerlegs.length)];
 	
 	$('#pet').css({"background-image" : "url("+pet.image+")" , "width" : pet.width , "height" : pet.height});
 	$('#owner-head').css({"background-image" : "url("+head.image+")" , "width" : head.width , "height" : head.height});
@@ -256,27 +336,38 @@ function create_customer() {
 	}
 	var game_container_width = $('#gamecontainer').width();
 	var owner_width = $('#owner').width();
+	
 	$('#pet-wrap').css("left" , "-"+pet.width+"px");
 	var customer_width = $('#customer').width();
 	$('#customer').css("left" , (game_container_width+pet.width+owner_width)-customer_width+"px");
-	//$("#customer").animate({left: '-=280'}, 3300, "linear",create_illness());
-	
-	
-	$( "#customer" ).animate({
-    left: "-=280",}, {duration: 3300,specialEasing: {width: "linear"},
+	var total_width = pet.width+customer_width;
+	var pixels_to_reach = (game_container_width-571); //571 is the tip of the selected syringe;
+	curr_customer.pixels_to_reach = pixels_to_reach;
+	curr_customer.money = create_money();
+	move_costumer(pixels_to_reach,false);
+	/*$( "#customer" ).animate({
+    left: "-="+pixels_to_reach}, {duration: 3300,specialEasing: {width: "linear"},
 		complete: function() {
 		  create_illness()
 		}
-	});
-	
-	//var illnesses = create_illness();
+	});*/
 		
 }
-
+	$("#start-next").click(function(){
+		create_customer();
+		$(this).hide();
+		$(this).html("Next");
+	});
+	$("#game-mode").click(function(){
+		(game_mode == "auto")?game_mode="manual":game_mode="auto";
+		$("#game-mode").html("mode: "+game_mode);
+	});
 	$(".syringe").click(function(){
 			
 		var codes = $(this).siblings(".code-wrap").children();
-	
+		$(this).siblings().css("display","none");
+		$(this).css("background-image","none");
+		$("#selected-syringe").show();
 		ok = true;
 		i=0;
 		
@@ -295,12 +386,14 @@ function create_customer() {
 		}
 		
 		if(ok){
-			alert("ok");
+			set_money(curr_customer.money,true);
 		}
 		else{
-			alert("error");
+			set_money(curr_customer.money,false);
 		}
-		//curr_illnesses = [];
+		clearInterval(time_repeat);
+		$('#time-limit').hide();
+		move_costumer(curr_customer.pixels_to_reach,true);
 	});
 		
 });
